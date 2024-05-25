c92b24f8208ab085526b74c3e9e2e393f2905523
Levi Sorum <levsor1@Levi-sin-MacBook-Pro.local>
2024-05-25 22:21:39 +0200

    Missing styling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,24 @@
 import "@/styles/globals.css";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex flex-col justify-between min-h-screen">
-        <header>Header</header>
-        <main className="flex flex-col flex-grow">{children}</main>
-        <footer>Footer</footer>
+        <header className="bg-lightgray  p-6 flex-wrap flex flex-row justify-between">
+          <Link href="/">Krogsveen technical case 2024</Link>
+          <nav className="flex flex-row gap-8">
+            <Link href="/">Home</Link>
+            <Link href="/dashboard">Dashboard</Link>
+          </nav>
+        </header>
+        <main className="flex flex-col flex-grow py-8 px-4 gap-8">
+          {children}
+        </main>
+        <footer className="bg-lightgray p-6 text-center">
+          Created by Levi Sørum
+        </footer>
       </body>
     </html>
   );
